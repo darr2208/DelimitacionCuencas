da7a4ee2bd2ba88c6e338b01eb21d7423c48b798
darr2208 <65313535+darr2208@users.noreply.github.com>
2025-06-30 17:33:37 -0500

    Update generar_shapefile.py

--- Cuencas/utils/generar_shapefile.py
+++ Cuencas/utils/generar_shapefile.py
@@ -2,6 +2,7 @@ import geopandas as gpd
 from shapely.geometry import box
 import os
 import zipfile
+import time
 
 def generar_shapefile_desde_bbox(minx, miny, maxx, maxy, carpeta_salida="data/outputs", nombre="cuenca"):
     if not os.path.exists(carpeta_salida):
@@ -10,7 +11,10 @@ def generar_shapefile_desde_bbox(minx, miny, maxx, maxy, carpeta_salida="data/ou
     nombre_base = os.path.join(carpeta_salida, nombre)
     geometria = box(minx, miny, maxx, maxy)
     gdf = gpd.GeoDataFrame({'nombre': [nombre]}, geometry=[geometria], crs="EPSG:4326")
-    gdf.to_file(f"{nombre_base}.shp")
+
+    gdf.to_file(f"{nombre_base}.shp", driver="ESRI Shapefile")
+
+    time.sleep(0.5)
 
     ruta_zip = f"{nombre_base}.zip"
     with zipfile.ZipFile(ruta_zip, 'w') as zipf:
