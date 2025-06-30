e4e6a84fe8074e00d8233a27551fe9c967696740
darr2208 <65313535+darr2208@users.noreply.github.com>
2025-06-30 17:32:26 -0500

    Update exportar_excel.py

--- Cuencas/utils/exportar_excel.py
+++ Cuencas/utils/exportar_excel.py
@@ -5,7 +5,14 @@ def exportar_a_excel(df_parametros, df_alturas, carpeta_salida="data/outputs", n
     if not os.path.exists(carpeta_salida):
         os.makedirs(carpeta_salida)
 
+    if not nombre_archivo.endswith(".xlsx"):
+        nombre_archivo += ".xlsx"
+
     ruta = os.path.join(carpeta_salida, nombre_archivo)
+
+    df_parametros["Parámetro"] = df_parametros["Parámetro"].astype(str)
+    df_parametros["Clasificación"] = df_parametros["Clasificación"].astype(str)
+
     with pd.ExcelWriter(ruta, engine='openpyxl') as writer:
         df_parametros.to_excel(writer, index=False, sheet_name='Parámetros Morfométricos')
         df_alturas.to_excel(writer, index=False, sheet_name='Clases Altitudinales')
